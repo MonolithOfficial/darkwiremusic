5169532501aad3676266808ba8e867dd61d6a4f0
DarkroomLaboratory <darkroomassociation@gmail.com>
2020-07-04 01:51:37 +0400

    Revert "restyling"
    This reverts commit bacdc921ca039414a351e4495f70a2a7b0608913.

--- src/App.js
+++ src/App.js
@@ -29,9 +29,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
-        <div id="backgroundHolder">
           <img id="mainLogo" src={darkwirecover} alt="darkwire"/>
-        </div>
           <Navbar />
           
           
